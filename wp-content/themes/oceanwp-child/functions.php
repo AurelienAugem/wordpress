e0e63993deb7ceee65059c2bc14dcd24e45d1887
AurelienAugem <130144174+AurelienAugem@users.noreply.github.com>
2023-05-17 12:00:19 +0200

    amélioration

--- wp-content/themes/oceanwp-child/functions.php
+++ wp-content/themes/oceanwp-child/functions.php
@@ -24,14 +24,15 @@ add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 // END ENQUEUE PARENT ACTION
 
 function selectVente(){
-    echo "<div class=\"buy\">";
-    echo "<input class=\"sold-items\" type=\"search\" placeholder=\"0\" >";
-    echo "<div class=\"plus-minus\">";
-    echo "<input class=\"plus-button\" type=\"submit\" value=\"+\">";
-    echo "<input class=\"minus-button\" type=\"submit\" value=\"-\">";
-    echo "</div>";
-    echo "<input class=\"ok-button\" type=\"submit\" value=\"OK\">";
-    echo "</div>";
+    echo 
+        "<div class=\"buy\">
+            <input class=\"sold-items\" type=\"search\" placeholder=\"0\">
+            <div class=\"plus-minus\">
+                <input class=\"plus-button\" type=\"submit\" value=\"+\">
+                <input class=\"minus-button\" type=\"submit\" value=\"-\">
+            </div>
+            <input class=\"ok-button\" type=\"submit\" value=\"OK\">
+        </div>";
 }
 
 add_shortcode('commande', 'selectVente');
